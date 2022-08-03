07bd0a6144957506335dfd954e100fb1d2eb0876
Jack Browne <jack.browne@ppb.com>
2022-08-03 10:08:00 +0100

    Lazy load NVM

--- zsh/configs/02-znap.zsh
+++ zsh/configs/02-znap.zsh
@@ -7,6 +7,8 @@ znap source ohmyzsh/ohmyzsh plugins/{git,sudo,command-not-found,aws}
 znap source zsh-users/zsh-completions
 znap source zsh-users/zsh-autosuggestions
 znap source zsh-users/zsh-syntax-highlighting
+export NVM_LAZY_LOAD=true
+znap source jackdbrowne/zsh-nvm
 
 # Select theme
 znap source romkatv/powerlevel10k
